89f117d9e99945e39efaa398b68cb846d2061afa
ItsRareToSeeIt <itsraretoseeit@proton.me>
2023-03-28 19:03:01 +0200

    fun with ChatGTP

--- cpp/cli/main.cpp
+++ cpp/cli/main.cpp
@@ -1,12 +1,18 @@
-#include <iostream>
+#include <cstdio>
+#include <string>
 
 int main() {
     int num1, num2 = 0;
+    char tmp[20];
+    
+    printf("Enter your name!\n");
+    scanf("%s", tmp);
+    std::string name = tmp;
+    printf("Greetings %s!\nEnter two numbers, one after another: \n", tmp);
+    scanf("%d", &num1);
+    scanf("%d", &num2);
 
-    std::cout << "Enter two numbers, one after another: " << std::endl;
-    std::cin >> num1;
-    std::cin >> num2;
+    printf("The sum of [%d] and [%d] is [%d].\n", num1, num2, num1 + num2);
 
-    std::cout << "The sum of the numbers: " << num1 << " + " << num2 << " = " << num1 + num2 << std::endl;
     return 0;
 }
