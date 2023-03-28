2d5384688587671b28d08cd02d24b68b769473a5
ItsRareToSeeIt <itsraretoseeit@proton.me>
2023-03-28 21:01:34 +0200

    still fun GPT

--- cpp/cli/main.cpp
+++ cpp/cli/main.cpp
@@ -1,7 +1,12 @@
+#include <memory>
 #include <iostream>
-#include <string>
 #include <vector>
 #include <map>
+
+
+std::unique_ptr<int> sum(int a, int b) {
+    return std::make_unique<int>(a+b);
+}
 
 int main() {
     int num1, num2;
@@ -10,13 +15,15 @@ int main() {
     std::cin >> num1;
     std::cin >> num2;
 
+    std::unique_ptr<int> ptr = sum(num1, num2);
+
     vec.push_back(num1);
     vec.push_back(num2);
 
     for (unsigned long i=0; i<vec.size(); i++){
         std::cout << vec[i] << std::endl;
     }
-    printf("The sum of [%d] and [%d] is [%d].\n", num1, num2, num1 + num2);
+    printf("The sum of [%d] and [%d] is [%d].\n", num1, num2, *ptr);
 
     std::map<std::string, int> aMap = {
         {"X", 20},
