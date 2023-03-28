9b5d20ab2d63277d3506bdba8afc8125f91b1b2d
ItsRareToSeeIt <itsraretoseeit@proton.me>
2023-03-28 20:25:31 +0200

    fun with ChatGTP

--- cpp/cli/main.cpp
+++ cpp/cli/main.cpp
@@ -1,18 +1,28 @@
-#include <cstdio>
+#include <iostream>
 #include <string>
+#include <vector>
+#include <map>
 
 int main() {
-    int num1, num2 = 0;
-    char tmp[20];
-    
-    printf("Enter your name!\n");
-    scanf("%s", tmp);
-    std::string name = tmp;
-    printf("Greetings %s!\nEnter two numbers, one after another: \n", tmp);
-    scanf("%d", &num1);
-    scanf("%d", &num2);
+    int num1, num2;
+    std::vector<int> vec;    
+    std::cout << "Enter two numbers!\n";
+    std::cin >> num1;
+    std::cin >> num2;
 
+    vec.push_back(num1);
+    vec.push_back(num2);
+
+    for (unsigned long i=0; i<vec.size(); i++){
+        std::cout << vec[i] << std::endl;
+    }
     printf("The sum of [%d] and [%d] is [%d].\n", num1, num2, num1 + num2);
+
+    std::map<std::string, int> aMap = {
+        {"X", 20},
+        {"Y", 21},
+        {"Z", 23}
+    };
 
     return 0;
 }
